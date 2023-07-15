e10984fe0693bfd1c0f4c5e18d0bbcb05cb9eb99
Carlos Alberto R <carlosalbertorios3@gmail.com>
2023-07-15 16:57:19 -0500

    🔐Validate login form

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -18,6 +18,8 @@ const Signup = () => {
   const [isLogin, setIsLogin] = useState(true);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
 
   const handleToggleCard = () => {
     setIsLogin(!isLogin);
@@ -25,6 +27,29 @@ const Signup = () => {
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
+    setEmailError("");
+    setPasswordError("");
+
+    if (!email) {
+      setEmailError("Email is required");
+      return;
+    }
+
+    if (!password) {
+      setPasswordError("Password is required");
+      return;
+    }
+
+    if (!/\S+@\S+\.\S+/.test(email)) {
+      setEmailError("Invalid email format");
+      return;
+    }
+
+    if (password.length < 6) {
+      setPasswordError("Password must be at least 6 characters long");
+      return;
+    }
+
     if (isLogin) {
       dispatch(loginWithEmailAndPassword(email, password)).then(() => {
         navigate("/home");
@@ -52,7 +77,7 @@ const Signup = () => {
     <div className="flex items-center justify-center my-8">
       <div className="bg-white p-8 rounded-lg shadow-lg w-full md:w-1/2 lg:w-1/3">
         <h1 className="text-2xl font-semibold mb-4">
-          {isLogin ? "Iniciar Sesión" : "Registrarse"}
+          {isLogin ? "Log In" : "Sign Up"}
         </h1>
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
@@ -62,39 +87,51 @@ const Signup = () => {
             <input
               type="email"
               id="email"
-              className="border border-gray-300 rounded-md px-3 py-2 w-full mt-1"
+              className={`border border-gray-300 rounded-md px-3 py-2 w-full mt-1 ${
+                emailError ? "border-red-500" : ""
+              }`}
               value={email}
               onChange={(e) => setEmail(e.target.value)}
             />
+            {emailError && (
+              <p className="text-red-500 text-sm mt-1">{emailError}</p>
+            )}
           </div>
+
           <div className="mb-4">
             <label htmlFor="password" className="text-sm font-medium">
-              Contraseña
+              Password
             </label>
             <input
               type="password"
               id="password"
-              className="border border-gray-300 rounded-md px-3 py-2 w-full mt-1"
+              className={`border border-gray-300 rounded-md px-3 py-2 w-full mt-1 ${
+                passwordError ? "border-red-500" : ""
+              }`}
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             />
+            {passwordError && (
+              <p className="text-red-500 text-sm mt-1">{passwordError}</p>
+            )}
           </div>
+
           <button
             type="submit"
             className="bg-blue-500 text-white rounded-md px-4 py-2 w-full font-semibold"
           >
-            {isLogin ? "Iniciar Sesión" : "Registrarse"}
+            {isLogin ? "Log In" : "Sign Up"}
           </button>
         </form>
         <div className="mt-4 text-center">
           <span className="text-sm">
-            {isLogin ? "¿No tienes una cuenta?" : "¿Ya tienes una cuenta?"}
+            {isLogin ? "Don't have an account?" : "Already have an account?"}
           </span>{" "}
           <button
             onClick={handleToggleCard}
             className="text-blue-500 hover:underline focus:outline-none"
           >
-            {isLogin ? "Regístrate" : "Inicia Sesión"}
+            {isLogin ? "Sign Up" : "Log In"}
           </button>
         </div>
         <div className="mt-6">
@@ -103,7 +140,7 @@ const Signup = () => {
             className="flex items-center justify-center bg-red-500 text-white rounded-md px-4 py-2 w-full font-semibold"
           >
             <FaGoogle className="mr-2 text-xl" />
-            Iniciar sesión con Google
+            Continue with Google
           </button>
 
           <button
@@ -111,7 +148,7 @@ const Signup = () => {
             className="flex items-center justify-center bg-gray-800 text-white rounded-md px-4 py-2 w-full mt-2 font-semibold"
           >
             <FaGithub className="mr-2 text-xl" />
-            Continuar con GitHub
+            Continue with GitHub
           </button>
         </div>
       </div>
